e7a6d82646d99a0ab6c46cdcfbfd93973fd0ae9f
Benedikt Hirmer <bhr@pomfort.com>
2014-02-27 15:41:06 +0100

    Initial Commit

---
+++ BHRExtensions/BHRExtensions.h
@@ -0,0 +1,13 @@
+//
+//  BHRExtensions.h
+//  BHRExtensions
+//
+//  Created by Benedikt Hirmer on 2/27/14.
+//  Copyright (c) 2014 HIRMER.me. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BHRExtensions : NSObject
+
+@end
---
+++ BHRExtensions/BHRExtensions.m
@@ -0,0 +1,13 @@
+//
+//  BHRExtensions.m
+//  BHRExtensions
+//
+//  Created by Benedikt Hirmer on 2/27/14.
+//  Copyright (c) 2014 HIRMER.me. All rights reserved.
+//
+
+#import "BHRExtensions.h"
+
+@implementation BHRExtensions
+
+@end
