991c17f07281dc9e3d303a08ef9765b242dbec7a
Benedikt Hirmer <benedikt@hirmer.me>
2019-12-28 19:58:58 +0400

    Use AlertController

--- BHRExtensions/Utilities/BHRCoreDataErrorManager.m
+++ BHRExtensions/Utilities/BHRCoreDataErrorManager.m
@@ -9,6 +9,7 @@
 #import "BHRCoreDataErrorManager.h"
 #import <UIKit/UIKit.h>
 #import "UIApplication+BHRVersionInfo.h"
+#import "UIViewController+BHRExtensions.h"
 
 @implementation BHRCoreDataErrorManager
 
@@ -17,25 +18,18 @@
 
 - (void)showErrorAlert
 {
-	dispatch_async(dispatch_get_main_queue(), ^
-				   {
-					   UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Unresolved error!", nil)
-																	   message:NSLocalizedString(@"An unresolved error occurred when trying to save your changes. \n\nPlease restart the app and try again.", nil)
-																	  delegate:self
-															 cancelButtonTitle:[NSString stringWithFormat:NSLocalizedString(@"Quit %@", nil), [UIApplication appName]]
-															 otherButtonTitles:nil];
-					   [alert show];
-				   });
-}
-
-#pragma mark - UIAlertViewDelegate
-
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
-{
-	if ([[alertView title] isEqualToString:NSLocalizedString(@"Unresolved error!", nil)])
-	{
-		abort();
-	}
+	dispatch_async(dispatch_get_main_queue(), ^{
+		NSString *title = NSLocalizedString(@"Unresolved error!", nil);
+		NSString *message = NSLocalizedString(@"An unresolved error occurred when trying to save your changes. \n\nPlease restart the app and try again.", nil);
+		UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+		
+		NSString *actionTitle = [NSString stringWithFormat:NSLocalizedString(@"Quit %@", nil), [UIApplication appName]];
+		[alertController addAction:[UIAlertAction actionWithTitle:actionTitle style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+			abort();
+		}]];
+		
+		[UIViewController.topViewController presentViewController:alertController animated:YES completion:nil];
+	});
 }
 
 
